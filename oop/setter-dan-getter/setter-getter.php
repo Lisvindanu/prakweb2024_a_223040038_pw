0cb64fc199baa916cce56c80d6a75c1d0f483bb1
Lisvindanu <Lisvindanu015@gmail.com>
2024-10-09 02:27:17 +0700

    Video setter-getter.php

--- oop/setter-dan-getter/setter-getter.php
+++ oop/setter-dan-getter/setter-getter.php
@@ -1,5 +1,5 @@
 <?php
-namespace visibility;
+namespace setterGetter;
 
 use gameOveriding\game;
 use Komikmikoveriding\komik;
@@ -7,13 +7,12 @@ use Komikmikoveriding\komik;
 require 'komik.php';
 require 'game.php';
 class produk {
-    public $judul,
+    private $judul,
          $penulis,
-         $penerbit;
+         $penerbit,
+        $harga;
 
     protected $diskon;
-
-    private  $harga;
 
     public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0) {
        $this->judul = $judul;
@@ -22,6 +21,44 @@ class produk {
        $this->harga = $harga;
     }
 
+    public function setJudul($judul) {
+        if(!is_string($judul)) {
+            throw new \Exception("Judul harus string");
+        }
+        $this->judul = $judul;
+    }
+
+    public function getJudul() {
+        return $this->judul;
+    }
+
+    public function getPenulis(): mixed
+    {
+        return $this->penulis;
+    }
+
+    public function setPenulis(mixed $penulis): void
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenerbit(): mixed
+    {
+        return $this->penerbit;
+    }
+
+    public function setPenerbit(mixed $penerbit): void
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
 
 
 
@@ -30,6 +67,10 @@ class produk {
     }
     public function getHarga() {
         return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function setHarga($harga) {
+        $this->harga = $harga;
     }
     public function getInfoProduct() {
         $str = " {$this->judul} | {$this->getLabel()} | (Rp. {$this->harga})";
@@ -48,7 +89,13 @@ echo $produk2->getInfoProduct();
 echo "<hr>";
 
 
-$produk2->setDiskon(40);
+$produk2->setDiskon(50);
 echo "Harga : " . $produk2->getHarga();
+echo "<hr>";
+$produk1->setJudul("Naruto Shippuden");
+echo $produk1->getJudul();
 
+echo "<hr>";
+$produk2->setPenulis("Lisvin");
+echo $produk2->getPenulis();
 
