51b26d98a4ee299521485096409842668b474f8e
Lisvindanu <Lisvindanu015@gmail.com>
2024-10-09 01:58:27 +0700

    Video accesModifier, merubah nama file class pada package / folder overriding yang awalnya bernama inheritance.php menjadi overriding.php

---
+++ oop/visibility/accesModifier.php
@@ -0,0 +1,54 @@
+<?php
+namespace InheritanceOverding;
+
+use gameOveriding\game;
+use Komikmikoveriding\komik;
+
+require 'komik.php';
+require 'game.php';
+class produk {
+    public $judul,
+         $penulis,
+         $penerbit;
+
+    protected $diskon;
+
+    private  $harga;
+
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0) {
+       $this->judul = $judul;
+       $this->penulis = $penulis;
+       $this->penerbit = $penerbit;
+       $this->harga = $harga;
+    }
+
+
+
+
+    public function getLabel(){
+        return "$this->judul, $this->penulis, $this->penerbit, $this->harga";
+    }
+    public function getHarga() {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+    public function getInfoProduct() {
+        $str = " {$this->judul} | {$this->getLabel()} | (Rp. {$this->harga})";
+        return $str;
+    }
+}
+
+
+$produk1 = new komik("Naruto", "Masashi Kishimoto", "Shounen Jump", 30000, 100 );
+$produk2 = new game("Uncharteed", "Neil Druckmann", "Shounen Jump", 250000, 50 );
+
+
+echo $produk1->getInfoProduct();
+echo "<br>";
+echo $produk2->getInfoProduct();
+echo "<hr>";
+
+
+$produk2->setDiskon(40);
+echo "Harga : " . $produk2->getHarga();
+
+
---
+++ oop/visibility/game.php
@@ -0,0 +1,24 @@
+<?php
+namespace gameOveriding;
+class game extends \InheritanceOverding\produk {
+
+    public $waktuMain;
+
+    public function __construct($judul , $penulis , $penerbit , $harga ,  $waktuMain) {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->waktuMain = $waktuMain;
+    }
+
+//    public function getHarga() {
+//        return $this->harga;
+//    }
+
+    public function setDiskon($diskon){
+        $this->diskon = $diskon;
+    }
+
+    public function getInfoProduct() {
+        $str = "Game : " . parent::getInfoProduct() . " - {$this->waktuMain} jam";
+        return $str;
+    }
+}
